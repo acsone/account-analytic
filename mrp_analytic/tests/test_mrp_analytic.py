29900a26ba460f11099ee0097a7bbccdc7fa6a78
Denis Roussel <denis.roussel@acsone.eu>
2018-06-11 17:22:13 +0200

    [FIX] mrp_analytic: openerp => odoo

--- mrp_analytic/tests/test_mrp_analytic.py
+++ mrp_analytic/tests/test_mrp_analytic.py
@@ -2,7 +2,7 @@
 # © 2015 Pedro M. Baeza - Antiun Ingeniería
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
-from openerp.tests import common
+from odoo.tests import common
 
 
 class TestMrpAnalytic(common.TransactionCase):
